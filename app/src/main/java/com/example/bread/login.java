761d6697ee57f55b88da2b837cc68f3b0903935d
noahjacknichols <noahjacknichols@gmail.com>
2019-10-25 17:59:17 -0400

    let's get this bread.

---
+++ app/src/main/java/com/example/bread/login.java
@@ -0,0 +1,14 @@
+package com.example.bread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle save  dInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
